fce3d8ee6df60d8b66fa830b5fa84725e9dcf505
laura-paullette <laura.paullette@student.moringaschool.com>
2023-06-23 11:53:07 +0300

    completed

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -14,7 +14,7 @@ function MovieForm() {
     female_director: false,
   });
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     fetch("/movies", {
       method: "POST",
@@ -22,9 +22,14 @@ function MovieForm() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    });
+      
+    const data = await response.json();
+    if (response.ok) {
+      console.log("Movie created:", data);
+    } else {
+      setErrors(data.errors);
+    }
   }
 
   function handleChange(e) {
@@ -125,7 +130,16 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+
+        {errors.length > 0 && (
+    <ul style={{ color: "red" }}>
+      {errors.map((error) => (
+        <li key={error}>{error}</li>
+      ))}
+    </ul>
+  )}
         <SubmitButton type="submit">Add Movie</SubmitButton>
+
       </form>
     </Wrapper>
   );
